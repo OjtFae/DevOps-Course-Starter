6b91305bf5f639bb3a73556f8e5921d464cf6d56
Oliver Taylor <Oliver.Taylor@etas.com>
2024-01-11 17:18:50 +0000

    Comments, return codes, env template

--- todo_app/app.py
+++ todo_app/app.py
@@ -9,6 +9,10 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
 
+# deliberate redirect (308) to '/index' to return the actual home page (helps out with lost people - no cost to implementation)
+@app.route('/')
+def hello():
+    return redirect(url_for('index'), 308) # using 308 allows browsers to cache the redirect
 
 @app.route('/index', methods=['GET'])
 def index():
@@ -18,5 +22,5 @@ def index():
 def add():
     if request.method == "POST":
         session_items.add_item(request.form.get("title"))
-        return redirect(url_for('index'))
+        return redirect(url_for('index'), 303) # 303 see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/303
     return render_template("add.html", itemsList = session_items.get_items())
