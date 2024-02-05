777e78d533bca0c115f505ab1876c23972b9ed72
Oliver Taylor <Oliver.Taylor@etas.com>
2024-02-05 11:24:37 +0000

    update poetry and environment variables examples

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,6 +4,12 @@ import os
 class Config:
     def __init__(self):
         """Base configuration variables."""
-        self.SECRET_KEY = os.environ.get('SECRET_KEY')
-        if not self.SECRET_KEY:
-            raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+        self.TRELLO_API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
+        self.TRELLO_API_KEY = os.environ.get('TRELLO_API_KEY')
+        self.TRELLO_BOARD_ID = os.environ.get('TRELLO_BOARD_ID')
+        self.TRELLO_STD_LIST = os.environ.get('TRELLO_STD_LIST')
+        #self.TRELLO_API_TOKEN = os.environ.get('TRELLO_API_TOKEN')
+        if not self.TRELLO_API_KEY:
+            raise ValueError("No TRELLO_API_KEY set . Did you follow the setup instructions?")
+        if not self.TRELLO_API_TOKEN:
+            raise ValueError("No TRELLO_API_TOKEN set . Did you follow the setup instructions?")
